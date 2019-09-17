c3d14453bde01347fd3ddbed8535c43e67659879
Timi <timi.rautamaki@gmail.com>
2019-09-17 15:17:16 +0300

    Add custom soong variables
    Change-Id: I7e33dbf3063b535ea55d3e8ee5156bb34f202d4b

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+        "custom/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -124,6 +126,9 @@ type variableProperties struct {
 		Device_support_hwfde_perf struct {
 			Cflags []string
 		}
+
+		// include Custom variables
+		Custom android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -233,6 +238,7 @@ type productVariables struct {
 	Override_rs_driver *string `json:",omitempty"`
 
 	DeviceKernelHeaders []string `json:",omitempty"`
+
 	DistDir             *string  `json:",omitempty"`
 
 	ExtraVndkVersions []string `json:",omitempty"`
@@ -242,6 +248,9 @@ type productVariables struct {
 	PgoAdditionalProfileDirs []string `json:",omitempty"`
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
+
+	// include Custom variables
+	Custom android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
@@ -302,6 +311,14 @@ func variableMutator(mctx BottomUpMutatorContext) {
 	variableValues := reflect.ValueOf(&a.variableProperties.Product_variables).Elem()
 	zeroValues := reflect.ValueOf(zeroProductVariables.Product_variables)
 
+	valStruct := reflect.ValueOf(mctx.Config().productVariables)
+
+	doVariableMutation(mctx, a, variableValues, zeroValues, valStruct)
+
+}
+
+func doVariableMutation(mctx BottomUpMutatorContext, a *ModuleBase, variableValues reflect.Value, zeroValues reflect.Value, valStruct reflect.Value) {
+
 	for i := 0; i < variableValues.NumField(); i++ {
 		variableValue := variableValues.Field(i)
 		zeroValue := zeroValues.Field(i)
@@ -309,8 +326,12 @@ func variableMutator(mctx BottomUpMutatorContext) {
 		property := "product_variables." + proptools.PropertyNameForField(name)
 
 		// Check that the variable was set for the product
-		val := reflect.ValueOf(mctx.Config().productVariables).FieldByName(name)
-		if !val.IsValid() || val.Kind() != reflect.Ptr || val.IsNil() {
+		val := valStruct.FieldByName(name)
+                if val.IsValid() && val.Kind() == reflect.Struct {
+			doVariableMutation(mctx, a, variableValue, zeroValue, val)
+                    continue
+        	} else if !val.IsValid() || val.Kind() != reflect.Ptr || val.IsNil() {
+
 			continue
 		}
 
